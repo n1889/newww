d861d11174eb07e4c9ed2209f867ba1eac8829b7
Aria Stewart <aredridel@dinhe.net>
2015-08-18 16:15:57 -0400

    Promisify User.prototype.signup
    - separate mail list signup as ensureMailingListState method
    - exposes the pending signup request on a pending property, which can be collected at response time to give error reports.

--- test/models/user.js
+++ test/models/user.js
@@ -19,7 +19,9 @@ beforeEach(function(done) {
   User = new (require("../../models/user"))({
     host: "https://user.com"
   });
-  spy = sinon.spy(function(a, b, c) {});
+  spy = sinon.spy(function(a, cb) {
+    cb();
+  });
   User.getMailchimp = function() {
     return {
       lists: {
@@ -582,12 +584,15 @@ describe("User", function() {
           resource: {
             npmweekly: "on"
           }
-        }, function(er, user) {
-          expect(er).to.not.exist();
+        }).then(function(user) {
+          return Promise.all(user.pending);
+        }).then(function(pending) {
           // userMock.done();
           expect(spy.calledWith(params)).to.be.true();
           done();
-        });
+        }).catch(function(err) {
+          done(err);
+        })
       });
 
       it('does not add the user to the mailing list when unchecked', function(done) {
